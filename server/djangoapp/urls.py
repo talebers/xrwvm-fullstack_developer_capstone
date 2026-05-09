594e2f950351f63d34c606273bfc5f83e1c65b77
Talebers <talebers@gmail.com>
2026-05-09 02:49:10 -0400

    Added Django car models and get_cars API

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='register', view=views.registration, name='register'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.views.decorators.csrf import csrf_exempt
+from .models import CarMake, CarModel
+from .populate import initiate
 import json
 import logging
 
@@ -19,18 +21,23 @@ def login_user(request):
     user = authenticate(username=username, password=password)
 
     if user is not None:
+
         login(request, user)
+
         data = {
             "userName": username,
             "status": "Authenticated"
         }
+
         return JsonResponse(data)
 
     else:
+
         data = {
             "userName": username,
             "status": "Failed"
         }
+
         return JsonResponse(data)
 
 
@@ -63,12 +70,14 @@ def registration(request):
     email_exist = False
 
     try:
+
         # Check if user already exists
         User.objects.get(username=username)
 
         username_exist = True
 
     except:
+
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
 
@@ -102,4 +111,26 @@ def registration(request):
         }
 
         return JsonResponse(data)
-        
+
+
+def get_cars(request):
+
+    count = CarMake.objects.filter().count()
+
+    print(count)
+
+    if(count == 0):
+        initiate()
+
+    car_models = CarModel.objects.select_related('car_make')
+
+    cars = []
+
+    for car_model in car_models:
+
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
+
+    return JsonResponse({"CarModels": cars})
